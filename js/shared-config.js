bc3687955912ca88194bea9967e3d57a8befaa12
Jhonn <jhonn@example.com>
2026-09-04 20:54:17 -0300

    Configura API compartilhada do historico

--- js/shared-config.js
+++ js/shared-config.js
@@ -1,1 +1,1 @@
-window.SHECARD_API_URL = 'COLE_A_URL_DO_GOOGLE_APPS_SCRIPT_AQUI';
+window.SHECARD_API_URL = 'https://script.google.com/macros/s/AKfycbx8eFFVdMdhNioHCeJUFflK2xk2iKJfX_ZDnOO5KdeEhOiR9bQl6IkTrnsRHpCu_iwG/exec';
